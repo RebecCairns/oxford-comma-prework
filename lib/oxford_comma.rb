5318ff391602eab9754b81c27184f6c79850052c
Rebecca Cairns <rebecca_cairns@yahoo.com>
2019-03-31 23:49:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,13 @@
 def oxford_comma(arr)
-  if(arr.length == 0)
-  newString = arr.join(", ")
+  if(arr.length == 1)
+    newString = arr(0)
+  end
+  if(arr.length == 2)
+    newString = arr.join(" and ")
+  end 
+  if(arr.length > 2)
+    
+    newString =
+  end   
   return newString
 end
